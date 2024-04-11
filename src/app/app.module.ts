ae4a0b6cedcd4df3496c9f16acdd6647212d7c47
Samuel Emeka <devsamuel0611@gmail.com>
2024-04-11 22:49:59 +0100

    finished home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FeaturesComponent } from './pages/features/features.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { SharedFeatureComponent } from './components/shared/shared-feature/shared-feature.component';
+import { NewsletterComponent } from './components/shared/newsletter/newsletter.component';
 
 // DEFINE THE ROUTES HERE
 
@@ -35,7 +36,8 @@ const routes: Routes = [
     FeaturesComponent,
     NotFoundComponent,
     FooterComponent,
-    SharedFeatureComponent
+    SharedFeatureComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
